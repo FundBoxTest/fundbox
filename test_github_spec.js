3db401500977832fb86e654b0e2584b3966a2187
FundBoxTest <40695555+FundBoxTest@users.noreply.github.com>
2018-07-01 15:30:43 +0300

    The final and working version

---
+++ test_github_spec.js
@@ -0,0 +1,15 @@
+var utils = require('../integration/utils/util') 
+//Test description
+describe("GitHub test", function(){
+  it("Login, search, logout functions",function(){
+      //calls login function from utils file
+    utils.loginToGithub("https://github.com/login","test-1234567890", "L8s57Dnj4J>@?B-;9")
+      //calls search function from utils file
+//    cy.pause()
+      utils.search("cypress.io")
+      //calls search repository function from utils file
+    utils.searchRepository("cypress-io/cypress-adapter-node")
+      //calls Logout function from utils file
+    utils.logoutFromGithub()
+  })
+})
